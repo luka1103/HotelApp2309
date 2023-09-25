2fa912e590a47f286578af0ef38e8117800bb8e4
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2023-09-25 10:39:03 +0200

    endpoint felix aangemaakt

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("Welkome bij onze hotel!")
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import felixprobeersels
+
+app = Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+@cross_origin()
+def helloWorld():
+    return "Hello, cross-origin-world!"
+
+@app.route("/felix")
+@cross_origin()
+def methodefelix():
+    return felixprobeersels.functiefelix()
