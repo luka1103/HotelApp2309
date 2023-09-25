b864b19259479d63df24ffc3194a44f89fb00c7e
FluxthisNL <jeroenhad19@gmail.com>
2023-09-25 10:51:03 +0200

    alle functies

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-print("Welkome bij onze hotel!")
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import felixprobeersels
+import lukaprobeersels
+import jeroenprobeer
+import zaidtest
+
+app = Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+@cross_origin()
+def helloWorld():
+    return "Hello, cross-origin-world!"
+
+@app.route("/felix")
+@cross_origin()
+def methodefelix():
+    return felixprobeersels.functiefelix()
+
+@app.route("/luka")
+@cross_origin()
+def methodeluka():
+    return felixprobeersels.functieluka()
+
+@app.route("/jeroen")
+@cross_origin()
+def methodejeroen():
+    return felixprobeersels.jeroenfunctie()
+
+@app.route("/zaid")
+@cross_origin()
+def methodezaid():
+    return felixprobeersels.functiezaid()
